72dbceb2eb8f4e06c1c1ec9ac511d08bf89a5e1e
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-23 23:09:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+
+function fetchImages() {
+  return fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then(json => renderImages(json));
+}
+
+function renderImages(json) {
+  const div = document.getElementById("dog-image-container");
+  json.forEach(image => {
+    let newImage = document.createElement('img');
+    newImage.src = image;
+    div.appendChild(newImage);
+  })
+}
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImages()
+})
